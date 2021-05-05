5011836d4f65cdc4706093c68f819e3ba18160ab
tusharvatsa32 <74168532+tusharvatsa32@users.noreply.github.com>
2021-05-05 09:36:34 -0400

    Add files via upload

---
+++ Code/plot_results.py
@@ -0,0 +1,15 @@
+def plot_results(pil_img, prob, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
+    
+plot_results(im, scores, boxes)
